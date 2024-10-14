c57acbc30c287b4a15091d27a5d932a8f806751e
Merlin-ki <159028744+Merlin-ki@users.noreply.github.com>
2024-10-15 01:51:15 +0200

    JWT authentication implemented and configuration customised
    "JWT authentication implemented and configuration customised".

--- core/cat/routes/settings.py
+++ core/cat/routes/settings.py
@@ -1,31 +1,65 @@
-from cat.auth.permissions import AuthPermission, AuthResource
-from cat.auth.connection import HTTPAuth
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
 from cat.db import models
 from cat.db import crud
-
+from .schemas import TokenData, User
+from .authentication import get_user
+import os
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SECRET_KEY = os.environ.get("CCAT_JWT_SECRET")
+ALGORITHM = "HS256"
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",   
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)   
+    if user is None:
+        raise credentials_exception
+    return user   
 
 @router.get("/")
 def get_settings(
     search: str = "",
-    stray=Depends(HTTPAuth(AuthResource.SETTINGS, AuthPermission.LIST)),
+    current_user: User = Depends(get_current_user),
 ):
     """Get the entire list of settings available in the database"""
 
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+        )
+
     settings = crud.get_settings(search=search)
-
     return {"settings": settings}
-
 
 @router.post("/")
 def create_setting(
     payload: models.SettingBody,
-    stray=Depends(HTTPAuth(AuthResource.SETTINGS, AuthPermission.WRITE)),
+    current_user: User = Depends(get_current_user),
 ):
     """Create a new setting in the database"""
+
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+        )
 
     # complete the payload with setting_id and updated_at
     payload = models.Setting(**payload.model_dump())
@@ -35,12 +69,18 @@ def create_setting(
 
     return {"setting": new_setting}
 
-
 @router.get("/{settingId}")
 def get_setting(
-    settingId: str, stray=Depends(HTTPAuth(AuthResource.SETTINGS, AuthPermission.READ))
+    settingId: str,
+    current_user: User = Depends(get_current_user),
 ):
     """Get the a specific setting from the database"""
+
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+        )
 
     setting = crud.get_setting_by_id(settingId)
     if not setting:
@@ -52,14 +92,19 @@ def get_setting(
         )
     return {"setting": setting}
 
-
 @router.put("/{settingId}")
 def update_setting(
     settingId: str,
     payload: models.SettingBody,
-    stray=Depends(HTTPAuth(AuthResource.SETTINGS, AuthPermission.EDIT)),
+    current_user: User = Depends(get_current_user),
 ):
     """Update a specific setting in the database if it exists"""
+
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+        )
 
     # does the setting exist?
     setting = crud.get_setting_by_id(settingId)
@@ -73,20 +118,24 @@ def update_setting(
 
     # complete the payload with setting_id and updated_at
     payload = models.Setting(**payload.model_dump())
-    payload.setting_id = settingId  # force this to be the setting_id
+    payload.setting_id = settingId
 
     # save to DB
     updated_setting = crud.update_setting_by_id(payload)
-
     return {"setting": updated_setting}
-
 
 @router.delete("/{settingId}")
 def delete_setting(
     settingId: str,
-    stray=Depends(HTTPAuth(AuthResource.SETTINGS, AuthPermission.DELETE)),
+    current_user: User = Depends(get_current_user),
 ):
     """Delete a specific setting in the database"""
+
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions",
+        )
 
     # does the setting exist?
     setting = crud.get_setting_by_id(settingId)
@@ -100,5 +149,4 @@ def delete_setting(
 
     # delete
     crud.delete_setting_by_id(settingId)
-
     return {"deleted": settingId}
